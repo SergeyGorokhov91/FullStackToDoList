62ccb238f0e9eaf63b06d7d285e1c9328db7d40c
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-21 13:40:22 +0300

    Add routes to App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import ListTaskComponent from "./components/ListTaskComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import {BrowserRouter as Router} from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <div className="container">
-        <ListTaskComponent/>
-      </div>
-      <FooterComponent/>
+      <Router>
+        <HeaderComponent/>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<ListTaskComponent/>}></Route>
+            <Route path="/tasks" element={<ListTaskComponent/>}></Route>
+          </Routes>
+        </div>
+        <FooterComponent/>
+      </Router>
     </div>
 
   );
