8bef088ddd2d8c308518f0ca73d3bea434af8996
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-21 10:57:58 +0300

    Add display of task list component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import ListTaskComponent from "./components/ListTaskComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+    <div className="container">
+      <ListTaskComponent/>
     </div>
   );
 }
