c636d5f5c83a83543e99115004209905306146f8
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-22 12:18:05 +0300

    add stylistic improvements and fix time zone display

--- react-frontend/src/components/ListTaskComponent.jsx
+++ react-frontend/src/components/ListTaskComponent.jsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import TaskService from "../services/TaskService";
+import {getTasks} from "../services/TaskService";
 import moment from "moment";
 import { useNavigate } from 'react-router-dom';
+
 
 
 function ListTaskComponent() {
@@ -9,7 +10,7 @@ function ListTaskComponent() {
   const navigate = useNavigate();
 
   useEffect(()=> {
-    TaskService().then(response => {
+    getTasks().then(response => {
       setTasks(response.data);
     });
   },[]);
@@ -42,8 +43,24 @@ function ListTaskComponent() {
                 task =>
                   <tr key={task.id}>
                     <td>{task.taskText}</td>
-                    <td>{moment.utc(task.addTime).format("DD MMM YYYY HH:mm")}</td>
-                    <td>{task.endTime ? moment.utc(task.endTime).format("DD MMM YYYY HH:mm"):""}</td>
+                    <td>
+                      <span className="date-style">
+                       {task.addDate ? moment.utc(task.addDate).local().format("DD MMM YY ") : "Error"}
+                      </span>
+                      <br/>
+                      <span className="time-style">
+                        {task.addDate ?moment.utc(task.addDate).local().format("HH:mm"):"not found"}
+                      </span>
+                    </td>
+                    <td>
+                      <span className="date-style">
+                        {task.endDate ? moment.utc(task.endDate).local().format("DD MMM YY ") : "-- --- --"}
+                      </span>
+                      <br/>
+                      <span className="time-style">
+                        {task.endDate ?moment.utc(task.endDate).local().format("HH:mm"):"-- --"}
+                      </span>
+                    </td>
                   </tr>
               )
             }
