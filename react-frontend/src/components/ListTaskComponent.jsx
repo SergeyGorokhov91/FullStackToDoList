cc0b0862da1c45039b09e0ba8897cb3f292a0cf3
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-24 12:52:37 +0300

    add checkboxes and improve visuals for tasks list

--- react-frontend/src/components/ListTaskComponent.jsx
+++ react-frontend/src/components/ListTaskComponent.jsx
@@ -6,31 +6,70 @@ import { useNavigate } from 'react-router-dom';
 
 
 function ListTaskComponent() {
-  const [tasks,setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [selectedTasks, setSelectedTasks] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(()=> {
-    getTasks().then(response => {
+  useEffect(() => {
+    getTasks().then((response) => {
       setTasks(response.data);
     });
-  },[]);
+  }, []);
 
   const addTask = () => {
-     navigate('/add-task/_add');
-  }
+    navigate("/add-task/_add");
+  };
 
   const editTask = (id) => {
-    navigate(`/add-task/${id}`)
-  }
+    navigate(`/add-task/${id}`);
+  };
 
   const deleteTask = (id) => {
     deleteTaskById(id).then(() => {
-      setTasks(tasks.filter(task => task.id !== id));
+      setTasks(tasks.filter((task) => task.id !== id));
     });
-  }
+  };
 
   function viewTask(id) {
-    navigate(`/view-task/${id}`)
+    navigate(`/view-task/${id}`);
+  }
+
+  function toggleTaskSelection(task) {
+    if (selectedTasks.includes(task.id)) {
+      setSelectedTasks((prevSelectedTasks) =>
+        prevSelectedTasks.filter((id) => id !== task.id)
+      );
+    } else {
+      setSelectedTasks((prevSelectedTasks) => [...prevSelectedTasks, task.id]);
+    }
+  }
+
+  function isTaskSelected(task) {
+    return selectedTasks.includes(task.id);
+  }
+
+  function buttonsSelectionRelative(task) {
+    if (!isTaskSelected(task)) {
+      return (
+          <button
+            onClick={() => editTask(task.id)}
+            className="btn btn-info"
+            style={{ marginLeft: "10px" }}
+          >
+            Update
+          </button>
+      );
+    } else {
+      return (
+          <button
+            onClick={() => deleteTask(task.id)}
+            className="btn btn-danger"
+            style={{ marginLeft: "10px" }}
+          >
+            Delete
+          </button>
+      );
+    }
   }
 
   return (
@@ -38,52 +77,66 @@ function ListTaskComponent() {
       <h2 className="text-center">Task List</h2>
       <div className="row">
         <div className="pl-0 pb-2">
-         <button className="btn btn-primary" onClick={addTask}> Add Todo Task</button>
+          <button className="btn btn-primary" onClick={addTask}>
+            Add Todo Task
+          </button>
         </div>
       </div>
       <div className="row">
         <table className="table table-striped table-bordered">
           <thead>
-            <tr>
-              <th>Task</th>
-              <th className="time-col">Created</th>
-              <th className="time-col">Do until</th>
-              <th className="action-col">Actions</th>
-            </tr>
+          <tr>
+            <th>Task</th>
+            <th className="time-col">Do until</th>
+            <th className="action-col">Actions</th>
+          </tr>
           </thead>
           <tbody>
-            {
-              tasks.map(
-                task =>
-                  <tr key={task.id}>
-                    <td>{task.taskText}</td>
-                    <td>
-                      <span className="date-style">
-                       {task.addDate ? moment.utc(task.addDate).local().format("DD MMM YY ") : "Error"}
-                      </span>
-                      <br/>
-                      <span className="time-style">
-                        {task.addDate ?moment.utc(task.addDate).local().format("HH:mm"):"not found"}
-                      </span>
-                    </td>
-                    <td>
-                      <span className="date-style">
-                        {task.endDate ? moment.utc(task.endDate).local().format("DD MMM YY ") : "-- --- --"}
-                      </span>
-                      <br/>
-                      <span className="time-style">
-                        {task.endDate ?moment.utc(task.endDate).local().format("HH:mm"):"-- --"}
-                      </span>
-                    </td>
-                    <td>
-                      <button onClick={()=> editTask(task.id)} className="btn btn-info">Update</button>
-                      <button onClick={()=> viewTask(task.id) } className="btn btn-info" style={{marginLeft:"10px"}}>View</button>
-
-                      <button onClick={()=> deleteTask(task.id)} className="btn btn-danger" style={{marginLeft:"10px"}}>Delete</button>
-                    </td>
-                  </tr>
-              )
-            }
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <input
+                    type="checkbox"
+                    checked={isTaskSelected(task)}
+                    onChange={() => toggleTaskSelection(task)}
+                  />
+                  <span
+                    style={{
+                      textDecoration: isTaskSelected(task)
+                        ? "line-through"
+                        : "none",
+                      paddingLeft: "5px",
+                    }}
+                  >
+                      {task.taskText}
+                    </span>
+                </div>
+              </td>
+              <td>
+                  <span className="date-style">
+                    {task.endDate
+                      ? moment.utc(task.endDate).local().format("DD MMM YY ")
+                      : "-- --- --"}
+                  </span>
+                <br />
+                <span className="time-style">
+                    {task.endDate
+                      ? moment.utc(task.endDate).local().format("HH:mm")
+                      : "-- --"}
+                  </span>
+              </td>
+              <td>
+                <button
+                  onClick={() => viewTask(task.id)}
+                  className="btn btn-info"
+                >
+                  View
+                </button>
+                {buttonsSelectionRelative(task)}
+              </td>
+            </tr>
+          ))}
           </tbody>
         </table>
       </div>
